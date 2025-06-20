e05947d134810f0ec7af239a0fb720339e5d5463
TownTowner <tanghelv@qq.com>
2025-06-21 01:01:16 +0800

    feat(api): 添加CORS中间件支持跨域请求
    添加FastAPI的CORSMiddleware以允许跨域请求，配置为允许所有来源、方法和头部

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from api.events import eventRT
 from api.db.session import init_db
-from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
-    # 关闭数据库连接
-    # session.close()
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(eventRT, prefix="/api/events")
 
 # docker: python:3.6.15-slim-buster
